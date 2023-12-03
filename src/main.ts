bf525b7df1347ded5c7400e4eace13c3f2eef817
almalmalm <almostkalinin@gmail.com>
2023-12-03 09:52:26 +0600

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://market-front-almalmalm.vercel.app/',
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
